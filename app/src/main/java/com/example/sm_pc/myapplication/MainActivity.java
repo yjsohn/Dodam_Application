6da5759fcad2a0a94a87ac6ee48880815704e919
Sue <suejeong97@naver.com>
2018-09-17 20:45:46 +0900

    sharedPreference baby name(succe)

--- app/src/main/java/com/example/sm_pc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sm_pc/myapplication/MainActivity.java
@@ -64,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user == null) {
-
                     startActivity(new Intent(MainActivity.this, LoginActivity.class));
                     finish();
                 }
